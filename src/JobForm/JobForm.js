d1be64c4d8e453ba2f5292e41c3610f13ccfd4c5
Ryan Seal <ryan@code.splrk.net>
2020-04-17 13:49:05 +0200

    Update styling of job form

--- src/JobForm/JobForm.js
+++ src/JobForm/JobForm.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import EventIcon from '@material-ui/icons/Event';
 import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -15,6 +16,15 @@ const useStyles = makeStyles(theme => ({
     },
     errorPaper: {
         backgroundColor: theme.palette.danger
+    },
+    company: {
+        color: 'gray',
+        '& input': {
+            textAlign: 'right'
+        }
+    },
+    postedDate: {
+        textAlign: 'left'
     }
 }));
 
@@ -28,7 +38,7 @@ const JobForm = ({ job, onSubmit, errors }) => {
 
     return (
         <Paper variant="outlined" square>
-            <Grid container direction="column">
+            <Grid container direction="column" spacing={3}>
                 { errors && errors.length > 0 && (
                     <Grid item>
                         <Paper className={classes.errorPaper}>
@@ -44,20 +54,28 @@ const JobForm = ({ job, onSubmit, errors }) => {
                         </Paper>
                     </Grid>
                 )}
-                <Grid item>
-                    <InputBase className={classes.jobTitle} value={fieldValues.title} onChange={changeField('title')} />
-                </Grid>
-                <Grid container item justify="flex-start">
-                    <Grid item xs>
-                        <InputBase className={classes.company} value={fieldValues.company} onChange={changeField('company')} />
+                <Grid item container direction="column" spacing={0}>
+                    <Grid item>
+                        <InputBase className={classes.jobTitle} value={fieldValues.title} onChange={changeField('title')} />
                     </Grid>
-                    <Grid item xs>
-                        <InputBase
-                            type="date"
-                            className={classes.postedDate}
-                            value={fieldValues.postedDate}
-                            onChange={changeField('postedDate')}
-                        />
+                    <Grid container item justify="flex-start">
+                        <Grid item xs>
+                            <InputBase
+                                fullWidth
+                                className={classes.company}
+                                value={fieldValues.company}
+                                onChange={changeField('company')}
+                            />
+                        </Grid>
+                        <Grid item xs>
+                            <EventIcon />
+                            <InputBase
+                                type="date"
+                                className={classes.postedDate}
+                                value={fieldValues.postedDate && fieldValues.postedDate.toLocaleDateString()}
+                                onChange={changeField('postedDate')}
+                            />
+                        </Grid>
                     </Grid>
                 </Grid>
                 <Grid item container spacing={1}>
