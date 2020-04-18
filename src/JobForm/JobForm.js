4c83e110777e10f42e7d37e51001fb8de205f190
Ryan Seal <ryan@code.splrk.net>
2020-04-18 07:14:35 +0200

    Add applied date field on Job form

--- src/JobForm/JobForm.js
+++ src/JobForm/JobForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import InputBase from '@material-ui/core/InputBase';
@@ -7,8 +7,10 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import EventIcon from '@material-ui/icons/Event';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { format, parseISO } from 'date-fns';
 
 const useStyles = makeStyles(theme => ({
     jobTitle: {
@@ -32,9 +34,21 @@ const JobForm = ({ job, onSubmit, errors }) => {
     const [fieldValues, setFields] = useState(job);
     const classes = useStyles();
 
-    const changeField = fieldName => event => {
-        setFields({ ...fieldValues, [fieldName]: event.target.value });
-    } 
+    useEffect(() => {
+        setFields(job);
+    }, [job]);
+
+    const changeField = (fieldName, transform = v => v) => event => {
+        const { [fieldName]: oldValue, ...oldValues } = fieldValues;
+        if (event.target.value) {
+            const newValue = transform(event.target.value);
+            setFields({ ...oldValues, [fieldName]: newValue });
+        } else {
+            setFields(oldValues);
+        }
+    }
+
+    const changeDateField = fieldName => changeField(fieldName, parseISO);
 
     return (
         <Paper variant="outlined" square>
@@ -72,8 +86,8 @@ const JobForm = ({ job, onSubmit, errors }) => {
                             <InputBase
                                 type="date"
                                 className={classes.postedDate}
-                                value={fieldValues.postedDate && fieldValues.postedDate.toLocaleDateString()}
-                                onChange={changeField('postedDate')}
+                                value={fieldValues.postedDate && format(fieldValues.postedDate, 'yyyy-MM-dd')}
+                                onChange={changeDateField('postedDate')}
                             />
                         </Grid>
                     </Grid>
@@ -96,6 +110,20 @@ const JobForm = ({ job, onSubmit, errors }) => {
                             value={fieldValues.applicationLink}
                             onChange={changeField('applicationLink')}
                         />
+                    </Grid>
+                    <Grid item container direction="column" xs={2}>
+                        <Grid item>
+                            {fieldValues.appliedDate && <><CheckCircleIcon color="action" /> Applied On</>}
+                            {!fieldValues.appliedDate && <><PriorityHighIcon color="error" />Haven't Applied Yet</>}
+                        </Grid>
+                        <Grid item>
+                            <InputBase
+                                type="date"
+                                fullWidth
+                                value={fieldValues.appliedDate ? format(fieldValues.appliedDate, 'yyyy-MM-dd') : ''}
+                                onChange={changeDateField('appliedDate')}
+                            />
+                        </Grid>
                     </Grid>
                 </Grid>
                 <Grid item>
